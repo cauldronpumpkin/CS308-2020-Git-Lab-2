7e5715e797b304567f5e85ce7dc6b405df57c933
blackviking991 <ishaanrock138@gmail.com>
2020-10-22 19:16:27 +0530

    Added plotting functionality and process data

--- main.py
+++ main.py
@@ -4,6 +4,58 @@ from tkinter import ttk
 from tkinter import filedialog, messagebox
 from matplotlib.figure import Figure 
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk) 
+from split_store import *
+
+num_of_lines, num_of_sentences, num_of_words = 0, 0, 0
+def process_data(path):
+    try:
+        global num_of_lines, num_of_sentences, num_of_words
+        (file, num_of_lines) = file_open(path)
+        (lists, num_of_sentences) = list_of_sentences(file)
+        (dict, num_of_words) =  split_and_store(lists)
+        sorted_dict = {k: v for k, v in sorted(dict.items(), key=lambda item: item[1])}
+    except:
+        return
+
+def plot(X, Y): 
+    plot_window = Toplevel()
+    plot_window.geometry("1900x1000")
+    plot_window.title("Plot for words")
+
+    fig = Figure(figsize = (19, 10), dpi = 100)     
+
+    plot1 = fig.add_subplot(212)
+    plot2 = fig.add_subplot(211)
+
+    plot1.bar(X[:100], Y[:100],align='edge', width=0.5)
+    plot1.set_title("Least occuring words")
+    for tick in plot1.get_xticklabels():
+        tick.set_rotation(90)
+    
+    plot2.bar(X[-100:], Y[-100:],align='edge', width=0.5)
+    plot2.set_title("Most occuring words")
+    for tick in plot2.get_xticklabels():
+        tick.set_rotation(90)
+    
+    fig.tight_layout()
+  
+    canvas = FigureCanvasTkAgg(fig, master = plot_window)   
+    canvas.draw() 
+   
+    canvas.get_tk_widget().pack() 
+  
+    toolbar = NavigationToolbar2Tk(canvas, plot_window) 
+    toolbar.update() 
+
+    canvas.get_tk_widget().pack()
+    plot_window.mainloop() 
+
+def refresh():
+    if (filepath.get() == ""):
+        messagebox.showerror("No File Found", "Please pick a file first.")  
+        return
+    else:
+        process_data(filepath.get())
 
 def clear():
     filepath.set("")
@@ -11,8 +63,6 @@ def clear():
     keywordpath.set("")
     keywordname.set("")
 
-def refresh():
-    pass
 
 def pick_file(*args):
     path =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("txt files","*.txt"),("all files","*.*")))
